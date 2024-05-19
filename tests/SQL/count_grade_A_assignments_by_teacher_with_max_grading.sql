967a5e8116317728f470ba7b663013ab652b0a8b
aditansh <aadityamahanta07@gmail.com>
2024-05-19 19:44:08 +0530

    feat: sql queries

--- tests/SQL/count_grade_A_assignments_by_teacher_with_max_grading.sql
+++ tests/SQL/count_grade_A_assignments_by_teacher_with_max_grading.sql
@@ -1,1 +1,11 @@
 -- Write query to find the number of grade A's given by the teacher who has graded the most assignments
+SELECT count(id)
+from assignments
+WHERE grade = 'A'
+    AND teacher_id = (
+        SELECT teacher_id
+        FROM assignments
+        GROUP BY teacher_id
+        ORDER BY count(id) DESC
+        LIMIT 1
+    );
--- tests/SQL/number_of_graded_assignments_for_each_student.sql
+++ tests/SQL/number_of_graded_assignments_for_each_student.sql
@@ -1,1 +1,4 @@
 -- Write query to get number of graded assignments for each student:
+SELECT count(id)
+FROM 'assignments'
+WHERE grade != '';
